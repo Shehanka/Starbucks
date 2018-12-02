e82145b9e6b2b0497f0216e0a164dd55dcc7b3c6
Shehanka <hcsperera@outlook.com>
2018-12-02 15:00:55 +0530

    Initial commit

---
+++ src/ui/controller/SplashScreenUIController.java
@@ -0,0 +1,18 @@
+package ui.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author chamodshehanka on 12/2/2018
+ * @project Starbucks
+ **/
+public class SplashScreenUIController implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
