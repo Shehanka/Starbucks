12f63e9737dd8ad31ef714d8f9e444635353f211
Shehanka <hcsperera@outlook.com>
2018-12-04 16:27:18 +0530

    Main Menu UI done

--- src/com/chamodshehanka/starbucks/ui/controller/MainMenuUIController.java
+++ src/com/chamodshehanka/starbucks/ui/controller/MainMenuUIController.java
@@ -1,9 +1,11 @@
 package com.chamodshehanka.starbucks.ui.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -23,16 +25,37 @@ public class MainMenuUIController implements Initializable {
 
     @FXML
     private void loadMakeOrderUI() {
-
+        AnchorPane makeOrderPane = null;
+        try {
+            makeOrderPane = FXMLLoader.load(getClass()
+                    .getResource("/com/chamodshehanka/starbucks/ui/fxml/MakeOrderUI.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        rootMainMenu.getChildren().setAll(makeOrderPane);
     }
 
     @FXML
     private void loadManageItemsUI() {
-
+        AnchorPane manageItemsPane = null;
+        try {
+            manageItemsPane = FXMLLoader.load(getClass()
+                    .getResource("/com/chamodshehanka/starbucks/ui/fxml/ManageItemsUI.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        rootMainMenu.getChildren().setAll(manageItemsPane);
     }
 
     @FXML
     private void loadTopItemsUI() {
-
+        AnchorPane topItemsPane = null;
+        try {
+            topItemsPane = FXMLLoader.load(getClass()
+                    .getResource("/com/chamodshehanka/starbucks/ui/fxml/TopItemsUI.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        rootMainMenu.getChildren().setAll(topItemsPane);
     }
 }
---
+++ src/com/chamodshehanka/starbucks/ui/controller/MakeOrderUIController.java
@@ -0,0 +1,18 @@
+package com.chamodshehanka.starbucks.ui.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author chamodshehanka on 12/4/2018
+ * @project Starbucks
+ **/
+public class MakeOrderUIController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
